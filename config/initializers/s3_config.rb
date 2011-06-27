c35b721b62fd20332eda9828e4491eb6f9e52496
Danigb <danigb@gmail.com>
2011-06-27 18:26:45 +0200

    s3 europe

--- config/initializers/s3_config.rb
+++ config/initializers/s3_config.rb
@@ -6,4 +6,5 @@ CarrierWave.configure do |config|
   config.s3_access_key_id = ENV['S3_KEY'] ? ENV['S3_KEY'] : local['access_key_id']
   config.s3_secret_access_key = ENV['S3_SECRET'] ? ENV['S3_SECRET'] : local['secret_access_key']
   config.s3_bucket = ENV['S3_BUCKET'] ? ENV['S3_BUCKET'] : local['bucket']
+  config.s3_region =  'eu-west-1'
 end
